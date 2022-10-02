07aa06eefd885d17aaca4389ed9f625abe834ec4
Asad Ali <asadali.code123@gmail.com>
2022-10-02 11:42:33 +0500

    ios files

---
+++ ios/payKES-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  paykes-Bridging-Header.h
+//  payKES
+//
+//  Created by Mapple.pk on 30/09/2022.
+//
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTConvert.h>
+#import <React/RCTUtils.h>
+
+#ifndef paykes_Bridging_Header_h
+#define paykes_Bridging_Header_h
+
+
+
+#endif /* paykes_Bridging_Header_h */
